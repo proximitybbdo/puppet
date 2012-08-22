6d14c69ef6963b80467ba6de23dea2ad04858e7e
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-08-22 10:45:54 +0200

    updating run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # install puppet
-echo '$$ Installing puppet'
 sudo apt-get -y -q install puppet-common
 
 # copy the initial setup manifest to the machine
-echo '$$ Copy the initial setup manifest to the machine'
 cd /etc/puppet/manifests
-sudo wget -N --quiet https://raw.github.com/proximitybbdo/puppet/master/manifests/init.pp
+sudo curl -s https://raw.github.com/proximitybbdo/puppet/master/manifests/init.pp -o
 
 # let puppet run the initial task, installing git
-echo '$$ Let puppet run the initial task, installing git'
 puppet apply /etc/puppet/manifests/init.pp
 
 # now clone our puppet repo on the machine and restart puppet
+sudo rm -r /etc/puppet/manifests
+sudo rm -r /etc/puppet/modules
+sudo git clone git://github.com/proximitybbdo/puppet.git /etc/puppet
