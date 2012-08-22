42a1d62e3c2672b0fb78e77dae20faaee3f5b335
Jeroen Bourgois <jeroenbourgois@gmail.com>
2012-08-22 10:14:32 +0200

    possible fix

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 # install puppet
-echo 'Installing puppet'
-sudo apt-get install puppet-common
+echo '$$ Installing puppet'
+sudo apt-get -y install puppet-common
 
 # copy the initial setup manifest to the machine
-echo 'copy the initial setup manifest to the machine'
-wget -N --quiet https://raw.github.com/proximitybbdo/puppet/master/manifests/init.pp; /etc/puppet/manifests/init.pp
+echo '$$ Copy the initial setup manifest to the machine'
+cd /etc/puppet/manifests
+sudo wget -N --quiet https://raw.github.com/proximitybbdo/puppet/master/manifests/init.pp
 
 # let puppet run the initial task, installing git
-echo 'let puppet run the initial task, installing git'
+echo '$$ Let puppet run the initial task, installing git'
 puppet apply /etc/puppet/manifests/init.pp
 
 # now clone our puppet repo on the machine and restart puppet
